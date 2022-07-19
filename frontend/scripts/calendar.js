c0c32009d4d7896563ca4b2610ce52db107c7220
Dovavivavi <szilagyiabel1@gmail.com>
2022-07-19 16:19:55 +0200

    edited js

---
+++ frontend/scripts/calendar.js
@@ -0,0 +1,26 @@
+function dayComponent(year, month, day) {
+    return `<div class="card">
+                <time datetime="YYYY">${year}</time>
+                <time datetime="MM">${month}</time>
+                <time datetime="DD">${day}</time>
+            </div>`
+}
+
+function monthComponent(month) {
+    let monthHtml = ""
+    for(let day = 1; day <= month.days; day++) {
+        monthHtml += dayComponent("2022", month.name, day)
+    }
+
+    return monthHtml
+}
+
+function init() {
+    const root = document.querySelector("#root")
+
+    const month = monthComponent(year[0])
+
+    root.insertAdjacentHTML("beforeend", firstDay)
+}
+
+init()
